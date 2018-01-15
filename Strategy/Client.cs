d6fb13ebae9105db9536b54b3b505a7bc21c497c
MaksimNikitin <max171192@gmail.com>
2018-01-15 19:32:28 +0300

    Added strategy pattern

---
+++ Strategy/Client.cs
@@ -0,0 +1,17 @@
+﻿namespace Strategy
+{
+    public class Client
+    {
+        private readonly IHolidayList _hollidayListStrategy;
+
+        public Client(IHolidayList strategy)
+        {
+            _hollidayListStrategy = strategy;
+        }
+
+        public bool CheckForHoliday(string date)
+        {
+            return _hollidayListStrategy.GetHolidays().Contains(date);
+        }
+    }
+}
